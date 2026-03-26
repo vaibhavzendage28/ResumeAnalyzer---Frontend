1c9344e51b0352f82cfeccb56b135375a4e9e4c5
vaibhavzendage28 <vaibhavzendage@gmial.com>
2026-03-27 01:38:42 +0530

    updated backend url

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: "https://resumeanalyzer-backend-lpjc.onrender.com",
   withCredentials: true,
 });
 
